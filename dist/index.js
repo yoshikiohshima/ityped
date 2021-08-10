e1ebe5c54c0233165a4864d8093e23f1692afb62
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2021-08-10 09:28:25 -0700

    add multiple cursor support

--- dist/index.js
+++ dist/index.js
@@ -17,112 +17,194 @@
  * @return {Promise}
  */
 var setProps = function setProps(_ref) {
-  var _ref$strings = _ref.strings,
-      strings = _ref$strings === undefined ? ['Put your strings here...', 'and Enjoy!'] : _ref$strings,
-      _ref$typeSpeed = _ref.typeSpeed,
-      typeSpeed = _ref$typeSpeed === undefined ? 100 : _ref$typeSpeed,
-      _ref$backSpeed = _ref.backSpeed,
-      backSpeed = _ref$backSpeed === undefined ? 50 : _ref$backSpeed,
-      _ref$backDelay = _ref.backDelay,
-      backDelay = _ref$backDelay === undefined ? 500 : _ref$backDelay,
-      _ref$startDelay = _ref.startDelay,
-      startDelay = _ref$startDelay === undefined ? 500 : _ref$startDelay,
-      _ref$cursorChar = _ref.cursorChar,
-      cursorChar = _ref$cursorChar === undefined ? '|' : _ref$cursorChar,
-      _ref$placeholder = _ref.placeholder,
-      placeholder = _ref$placeholder === undefined ? false : _ref$placeholder,
-      _ref$showCursor = _ref.showCursor,
-      showCursor = _ref$showCursor === undefined ? true : _ref$showCursor,
-      _ref$disableBackTypin = _ref.disableBackTyping,
-      disableBackTyping = _ref$disableBackTypin === undefined ? false : _ref$disableBackTypin,
-      _ref$onFinished = _ref.onFinished,
-      onFinished = _ref$onFinished === undefined ? function () {} : _ref$onFinished,
-      _ref$loop = _ref.loop,
-      loop = _ref$loop === undefined ? true : _ref$loop;
-  return {
-    strings: strings,
-    typeSpeed: typeSpeed,
-    backSpeed: backSpeed,
-    cursorChar: cursorChar,
-    backDelay: backDelay,
-    placeholder: placeholder,
-    startDelay: startDelay,
-    showCursor: showCursor,
-    loop: loop,
-    disableBackTyping: disableBackTyping,
-    onFinished: onFinished
-  };
+    var _ref$typeSpeed = _ref.typeSpeed,
+        typeSpeed = _ref$typeSpeed === undefined ? 100 : _ref$typeSpeed,
+        _ref$backSpeed = _ref.backSpeed,
+        backSpeed = _ref$backSpeed === undefined ? 50 : _ref$backSpeed,
+        _ref$backDelay = _ref.backDelay,
+        backDelay = _ref$backDelay === undefined ? 500 : _ref$backDelay,
+        _ref$startDelay = _ref.startDelay,
+        startDelay = _ref$startDelay === undefined ? 500 : _ref$startDelay,
+        _ref$cursorChar = _ref.cursorChar,
+        cursorChar = _ref$cursorChar === undefined ? '|' : _ref$cursorChar,
+        _ref$showCursor = _ref.showCursor,
+        showCursor = _ref$showCursor === undefined ? true : _ref$showCursor,
+        _ref$disableBackTypin = _ref.disableBackTyping,
+        disableBackTyping = _ref$disableBackTypin === undefined ? false : _ref$disableBackTypin,
+        _ref$onFinished = _ref.onFinished,
+        onFinished = _ref$onFinished === undefined ? function () {} : _ref$onFinished,
+        _ref$loop = _ref.loop,
+        loop = _ref$loop === undefined ? true : _ref$loop;
+    return {
+        typeSpeed: typeSpeed,
+        backSpeed: backSpeed,
+        cursorChar: cursorChar,
+        backDelay: backDelay,
+        startDelay: startDelay,
+        showCursor: showCursor,
+        loop: loop,
+        disableBackTyping: disableBackTyping,
+        onFinished: onFinished
+    };
 };
 
 var init = function init(element, properties) {
-  var i = 0,
-      l = void 0,
-      STRINGS_TO_ITERATE = void 0;
+    var i = 0,
+        l = void 0,
+        WORK_TO_DO = void 0;
+    var state = {}; // {id: {pos: number}}
+    var text = "";
 
-  var typewrite = function typewrite(strings, props) {
-    if (i === l) if (props.loop) i = 0;
-    setTimeout(function () {
-      typeString(strings[i], props);
-    }, props.startDelay);
-  };
+    var typewrite = function typewrite(work, props) {
+        if (i === 1) if (props.loop) i = 0;
+        setTimeout(function () {
+            typeString(work, props);
+        }, props.startDelay);
+    };
 
-  var typeString = function typeString(str, props) {
-    var index = 0,
-        strLen = str.length;
-    var intervalID = setInterval(function () {
-      props.placeholder ? element.placeholder += str[index] : element.textContent += str[index];
-      if (++index === strLen) return onStringTyped(intervalID, props);
-    }, props.typeSpeed);
-  };
+    var typeString = function typeString(work, props) {
+        var index = 0;
+        var workLen = work.length;
+        var intervalID = setInterval(function () {
+            doWork(work[index++], props);
+            if (index === workLen) {
+                onStringTyped(intervalID, props);
+            }
+        }, props.typeSpeed);
+    };
 
-  var onStringTyped = function onStringTyped(id, props) {
-    clearInterval(id);
-    if (props.disableBackTyping && i === l - 1) {
-      return props.onFinished();
-    }
-    if (!props.loop && i === l - 1) {
-      return props.onFinished();
-    }
-    setTimeout(function () {
-      return eraseString(props);
-    }, props.backDelay);
-  };
+    var makeSpans = function makeSpans() {
+        var result = [];
+        for (var _i = 0; _i < text.length; _i++) {
+            var span = document.createElement("span");
+            span.textContent = text[_i];
+            result.push(span);
+        }
+        return result;
+    };
 
-  var eraseString = function eraseString(props) {
-    var str = props.placeholder ? element.placeholder : element.textContent,
-        strLen = str.length;
-    var intervalID = setInterval(function () {
-      props.placeholder ? element.placeholder = element.placeholder.substr(0, --strLen) : element.textContent = str.substr(0, --strLen);
-      if (strLen === 0) return onStringErased(intervalID, props);
-    }, props.backSpeed);
-  };
+    var doWork = function doWork(workArray, props) {
+        workArray.forEach(function (work) {
+            var id = "" + work.id;
+            if (!state[id]) {
+                state[id] = { pos: 0 };
+            }
 
-  var onStringErased = function onStringErased(id, props) {
-    clearInterval(id);
-    ++i;
-    typewrite(STRINGS_TO_ITERATE, props);
-  };
+            if (work.append !== undefined) {
+                var before = text.slice(0, state[id].pos);
+                var after = text.slice(state[id].pos);
+                text = before + work.append + after;
+                state[id].pos++;
+                Object.keys(state).forEach(function (key) {
+                    if (key === id) {
+                        return;
+                    }
+                    if (state[key].pos >= state[id].pos) state[key].pos++;
+                });
+            }
+            if (work.goto !== undefined) {
+                state[id].pos = work.goto;
+            }
+            if (work.backspace !== undefined) {
+                var _before = text.slice(0, state[id].pos - 1);
+                var _after = text.slice(state[id].pos);
+                text = _before + _after;
+                state[id].pos--;
+                Object.keys(state).forEach(function (key) {
+                    if (key === id) {
+                        return;
+                    }
+                    if (state[key].pos >= state[id].pos) state[key].pos--;
+                });
+            }
+        });
+        while (element.lastChild) {
+            element.lastChild.remove();
+        }
+        makeSpans().forEach(function (s) {
+            return element.appendChild(s);
+        });
+        positionCursors();
+    };
 
-  var setCursor = function setCursor(element, props) {
-    var cursorSpan = document.createElement('span');
-    cursorSpan.classList.add('ityped-cursor');
-    cursorSpan.textContent = '|';
-    cursorSpan.textContent = props.cursorChar;
-    element.insertAdjacentElement('afterend', cursorSpan);
-  };
+    var onStringTyped = function onStringTyped(id, props) {
+        clearInterval(id);
+        if (props.disableBackTyping) {
+            return props.onFinished();
+        }
+        if (!props.loop) {
+            return props.onFinished();
+        }
+        setTimeout(function () {
+            return eraseString(props);
+        }, props.backDelay);
+    };
 
-  var startTyping = function startTyping(prop) {
-    var props = setProps(prop || {});
-    var strings = props.strings;
-    STRINGS_TO_ITERATE = strings;
-    l = strings.length;
-    if (typeof element === "string") element = document.querySelector(element);
-    if (props.showCursor) setCursor(element, props);
-    typewrite(strings, props);
-  };
+    var eraseString = function eraseString(props) {
+        var w = [{ id: 0, backspace: 1 }];
+        var intervalID = setInterval(function () {
+            doWork(w, props);
+            if (text.length === 0) return onStringErased(intervalID, props);
+        }, props.backSpeed);
+    };
 
-  return startTyping(properties);
+    var clearState = function clearState() {
+        Object.keys(state).forEach(function (key) {
+            var s = state[key];
+            if (s.cursor) {
+                s.cursor.remove();
+                s.cursor = null;
+            }
+        });
+        state = {};
+        text = "";
+    };
+
+    var onStringErased = function onStringErased(id, props) {
+        clearInterval(id);
+        ++i;
+        clearState();
+        typewrite(WORK_TO_DO, props);
+    };
+
+    var positionCursors = function positionCursors() {
+        var eRect = element.parentNode.getBoundingClientRect();
+        Object.keys(state).forEach(function (key) {
+            var s = state[key];
+            if (!s.cursor) {
+                s.cursor = document.createElement('span');
+                s.cursor.classList.add('ityped-cursor');
+                s.cursor.classList.add("ityped-cursor-" + key);
+                s.cursor.textContent = "|";
+                s.cursor.width = "0";
+                s.cursor.style.position = "relative";
+                s.cursor.style.fontWeight = 800;
+                var cursors = document.querySelector("#ityped-cursors");
+                cursors.appendChild(s.cursor);
+            }
+            var span = s.pos === 0 ? null : element.childNodes[s.pos - 1];
+            var rect = span ? span.getBoundingClientRect() : { right: 0 };
+            var cRect = s.cursor.getBoundingClientRect();
+            var keyNum = parseFloat(key);
+            s.cursor.style.left = rect.right - cRect.width * 0.5 - keyNum * cRect.width - eRect.left + "px";
+            s.cursor.style.top = rect.top - eRect.top + "px";
+            s.cursor.style.height = eRect.height + "px";
+        });
+    };
+
+    var startTyping = function startTyping(prop) {
+        var props = setProps(prop || {});
+        var work = prop.work;
+
+        WORK_TO_DO = work;
+        if (typeof element === "string") element = document.querySelector(element);
+        // if (props.showCursor) setCursor(element, props)
+        typewrite(WORK_TO_DO, props);
+    };
+    return startTyping(properties);
 };
+
+// window.ityped = {init};
 
 exports.init = init;
 
--- src/index.js
+++ src/index.js
@@ -5,24 +5,20 @@
  * @return {Promise}
  */
 const setProps = ({
-  strings = ['Put your strings here...', 'and Enjoy!'],
   typeSpeed = 100,
   backSpeed = 50,
   backDelay = 500,
   startDelay = 500,
   cursorChar = '|',
-  placeholder = false,
   showCursor = true,
   disableBackTyping = false,
   onFinished = function () { },
   loop = true
 }) => ({
-  strings,
   typeSpeed,
   backSpeed,
   cursorChar,
   backDelay,
-  placeholder,
   startDelay,
   showCursor,
   loop,
@@ -31,69 +27,153 @@ const setProps = ({
 })
 
 export const init = (element, properties) => {
-  let i = 0, l, STRINGS_TO_ITERATE;
-
-  const typewrite = (strings, props) => {
-    if (i === l)
-      if (props.loop) i = 0;
-    setTimeout(() => { typeString(strings[i], props); }, props.startDelay);
-  }
-
-  const typeString = (str, props) => {
-    let index = 0,
-      strLen = str.length;
-    let intervalID = setInterval(() => {
-      props.placeholder ? element.placeholder += str[index] : element.textContent += str[index];
-      if (++index === strLen) return onStringTyped(intervalID, props);
-    }, props.typeSpeed);
-  }
-
-  const onStringTyped = (id, props) => {
-    clearInterval(id);
-    if (props.disableBackTyping && i === l - 1) {
-      return props.onFinished()
+    let i = 0, l, WORK_TO_DO;
+    let state = {}; // {id: {pos: number}}
+    let text = "";
+    
+    const typewrite = (work, props) => {
+        if (i === 1)
+            if (props.loop) i = 0;
+        setTimeout(() => { typeString(work, props); }, props.startDelay);
     }
-    if (!props.loop && i === l - 1) {
-      return props.onFinished();
+
+    const typeString = (work, props) => {
+        let index = 0;
+        let workLen = work.length;
+        let intervalID = setInterval(() => {
+            doWork(work[index++], props);
+            if (index === workLen) {
+                onStringTyped(intervalID, props);
+            }
+        }, props.typeSpeed);
     }
-    setTimeout(() => eraseString(props), props.backDelay);
-  }
 
-  const eraseString = (props) => {
-    let str = props.placeholder ? element.placeholder : element.textContent,
-      strLen = str.length;
-    let intervalID = setInterval(() => {
-      props.placeholder
-        ? element.placeholder = element.placeholder.substr(0, --strLen)
-        : element.textContent = str.substr(0, --strLen);
-      if (strLen === 0) return onStringErased(intervalID, props);
-    }, props.backSpeed);
-  }
+    const makeSpans = () => {
+        let result = [];
+        for (let i = 0; i < text.length; i++) {
+            let span = document.createElement("span");
+            span.textContent = text[i];
+            result.push(span);
+        }
+        return result;
+    }
 
-  const onStringErased = (id, props) => {
-    clearInterval(id);
-    ++i;
-    typewrite(STRINGS_TO_ITERATE, props);
-  }
+    const doWork = (workArray, props) => {
+        workArray.forEach(work => {
+            let id = `${work.id}`;
+            if (!state[id]) {
+                state[id] = {pos: 0};
+            }
 
-  const setCursor = (element, props) => {
-    let cursorSpan = document.createElement('span');
-    cursorSpan.classList.add('ityped-cursor');
-    cursorSpan.textContent = '|';
-    cursorSpan.textContent = props.cursorChar;
-    element.insertAdjacentElement('afterend', cursorSpan);
-  }
+            if (work.append !== undefined) {
+                let before = text.slice(0, state[id].pos);
+                let after = text.slice(state[id].pos);
+                text = before + work.append + after;
+                state[id].pos++;
+                Object.keys(state).forEach(key => {
+                    if (key === id) {return;}
+                    if (state[key].pos >= state[id].pos)
+                        state[key].pos++;
+                });
+            }
+            if (work.goto !== undefined) {
+                state[id].pos = work.goto;
+            }
+            if (work.backspace !== undefined) {
+                let before = text.slice(0, state[id].pos - 1);
+                let after = text.slice(state[id].pos);
+                text = before + after;
+                state[id].pos--;
+                Object.keys(state).forEach(key => {
+                    if (key === id) {return;}
+                    if (state[key].pos >= state[id].pos)
+                        state[key].pos--;
+                });
+                
+            }
+        });
+        while (element.lastChild) {
+            element.lastChild.remove();
+        }
+        makeSpans().forEach(s => 
+            element.appendChild(s)
+        )
+        positionCursors();
+    }
+ 
+    const onStringTyped = (id, props) => {
+        clearInterval(id);
+        if (props.disableBackTyping) {
+            return props.onFinished()
+        }
+        if (!props.loop) {
+            return props.onFinished();
+        }
+        setTimeout(() => eraseString(props), props.backDelay);
+    }
 
+    const eraseString = (props) => {
+        let w = [{id: 0, backspace: 1}]
+        let intervalID = setInterval(() => {
+            doWork(w, props);
+            if (text.length === 0) return onStringErased(intervalID, props);
+        }, props.backSpeed);
+    }
 
-  const startTyping = (prop) => {
-    let props = setProps(prop || {})
-    let strings = props.strings
-    STRINGS_TO_ITERATE = strings
-    l = strings.length
-    if (typeof element === "string") element = document.querySelector(element)
-    if (props.showCursor) setCursor(element, props)
-    typewrite(strings, props)
-  }
+    const clearState = () => {
+        Object.keys(state).forEach(key => {
+            let s = state[key];
+            if (s.cursor) {
+                s.cursor.remove();
+                s.cursor = null;
+            }
+        });
+        state = {};
+        text = "";
+    }
 
-  return startTyping(properties)
+    const onStringErased = (id, props) => {
+        clearInterval(id);
+        ++i;
+        clearState();
+        typewrite(WORK_TO_DO, props);
+    }
+
+    const positionCursors = () => {
+        let eRect = element.parentNode.getBoundingClientRect();
+        Object.keys(state).forEach(key => {
+            let s = state[key];
+            if (!s.cursor) {
+                s.cursor = document.createElement('span');
+                s.cursor.classList.add('ityped-cursor');
+                s.cursor.classList.add(`ityped-cursor-${key}`);
+                s.cursor.textContent = "|";
+                s.cursor.width = "0";
+                s.cursor.style.position = "relative";
+                s.cursor.style.fontWeight = 800;
+                let cursors = document.querySelector("#ityped-cursors");
+                cursors.appendChild(s.cursor);
+            }
+            let span = s.pos === 0 ? null : element.childNodes[s.pos - 1];
+            let rect = span ? span.getBoundingClientRect() : {right: 0}
+            let cRect = s.cursor.getBoundingClientRect();
+            let keyNum = parseFloat(key);
+            s.cursor.style.left = `${rect.right - (cRect.width * 0.5) - (keyNum * cRect.width) - eRect.left}px`;
+            s.cursor.style.top = `${rect.top - eRect.top}px`;
+            s.cursor.style.height = `${eRect.height}px`;
+        })
+    }
+        
+    const startTyping = (prop) => {
+        let props = setProps(prop || {})
+        let work = prop.work
+
+        WORK_TO_DO = work;
+        if (typeof element === "string") element = document.querySelector(element)
+        // if (props.showCursor) setCursor(element, props)
+        typewrite(WORK_TO_DO, props)
+    }
+    return startTyping(properties)
 }
+
+// window.ityped = {init};
